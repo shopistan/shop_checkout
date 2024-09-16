99a437c9adccd403fa719a0be28f026cd01c84a7
Hamad0301 <hamad.rashid@xstak.com>
2024-09-16 13:19:23 +0500

    DEVOPS-3248:hotfix:add buildspec and servicefile

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -9,8 +9,8 @@ import { restResources } from "@shopify/shopify-api/rest/admin/2024-07";
 import prisma from "./db.server";
 
 const shopify = shopifyApp({
-  apiKey: process.env.3ec471a28fb9b2749c753c0e429953d6,
-  apiSecretKey: process.env.fd37d7c0eb7226f035d084e778cd0ac4,
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.July24,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
